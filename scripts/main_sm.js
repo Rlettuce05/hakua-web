0d7ea94a16b62b2273b63170b1dcb9ab72de3583
syunsuke okamura <syunrobin@gmail.com>
2020-12-31 13:39:54 +0900

    topの動画をwebmに変更

--- scripts/main_sm.js
+++ scripts/main_sm.js
@@ -1,8 +1,2 @@
 if (navigator.userAgent.match(/(iPhone|Android|iPad|iPod)/)) {
-    $(() => {
-        //topのmarginを設定する
-        var width = $(window).width();
-
-        $("#pagetop").css('margin-bottom', `${(width/16)*9-112}px`);
-    });
 }
